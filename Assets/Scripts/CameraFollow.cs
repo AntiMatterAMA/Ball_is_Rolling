f21e8295a02a3a7c2b1a31e36d7d5ce11f782221
AntiMatterAMA <david.riedler@gelse.net>
2017-10-04 19:51:02 +0200

    Unfunktional!
    Collider wurden gefixt.
    Größen Wurden gefixt.
    Kamerafollow muss noch integriert werden.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+  public Transform TargetTransform;
+  public float CameraFollowSpeed = 3f;
+  public Vector3 Offset = new Vector3(0f,30f,0f);
+
+  private Vector3 _thisPosition;
+  private Vector3 _repositionTargetDirection;
+
+  public void Start()
+  {
+    _thisPosition = transform.position;
+  }
+
+  private void FixedUpdate()
+  {
+    if (TargetTransform == null)
+      return;
+
+    var targetTransformPosition = TargetTransform.position + Offset;
+    _repositionTargetDirection.x = targetTransformPosition.x - _thisPosition.x;
+    _repositionTargetDirection.y = targetTransformPosition.y - _thisPosition.y;
+    _repositionTargetDirection.z = targetTransformPosition.z - _thisPosition.z;
+
+    _thisPosition = _thisPosition + (_repositionTargetDirection.normalized * _repositionTargetDirection.magnitude * CameraFollowSpeed * Time.deltaTime);
+
+    transform.position = _thisPosition;
+  }
+}
